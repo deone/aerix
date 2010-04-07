21398ddbfa0200fc97b6a7dda494f2d7dbd9559c
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-04-07 12:11:48 +0100

    Eliminate font redundancies. Image links in includes/_content.home.inc.php should link to services/index.php for now.

--- includes/_content.home.inc.php
+++ includes/_content.home.inc.php
@@ -4,13 +4,18 @@
                                 	<table cellspacing="0" cellpadding="0">
                                     <tr>
                                     	<td align="center" valign="middle" class="itemPic_cell">
-                                            <a href="services.php?i=1"><img src="images/itemPic_1.jpg" class="blockDisplay" /></a>
+                                            <!--<a href="services.php?i=1"><img src="images/itemPic_1.jpg" class="blockDisplay" /></a>-->
+                                            <a href="services/index.php"><img src="images/itemPic_1.jpg" class="blockDisplay" /></a>
                                         </td>
                                         <td align="center" valign="middle" class="itemPic_cell">
-                                            <a href="services.php?i=2"><img src="images/itemPic_2.jpg" class="blockDisplay" /></a>
+                                            <!--<a href="services.php?i=2"><img src="images/itemPic_2.jpg" class="blockDisplay" /></a>-->
+                                            <a href="services/index.php"><img src="images/itemPic_2.jpg" class="blockDisplay" /></a>
+                                        </td>
                                         </td>
                                         <td align="center" valign="middle" class="itemPic_cell">
-                                            <a href="services.php?i=3"><img src="images/itemPic_3.jpg" class="blockDisplay" /></a>
+                                            <!--<a href="services.php?i=3"><img src="images/itemPic_3.jpg" class="blockDisplay" /></a>-->
+                                            <a href="services/index.php"><img src="images/itemPic_3.jpg" class="blockDisplay" /></a>
+                                        </td>
                                         </td>
                                     </tr>
                                     </table>
