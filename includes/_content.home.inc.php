fb69d3ec3f2e449a8627bc1ff84d7ee408e42ffa
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-05-17 13:54:47 +0100

    Valid markup for home and services content too.

--- includes/_content.home.inc.php
+++ includes/_content.home.inc.php
@@ -1,24 +1,7 @@
-<table cellspacing="0" cellpadding="0" width="100%">
-                            <tr>
-                            	<td align="center" valign="top">
-                                	<table cellspacing="0" cellpadding="0">
-                                    <tr>
-                                    	<td align="center" valign="middle" class="itemPic_cell">
-                                            <!--<a href="services.php?i=1"><img src="images/itemPic_1.jpg" class="blockDisplay" /></a>-->
-                                            <a href="services/index.php"><img src="images/itemPic_1.jpg" class="blockDisplay" /></a>
-                                        </td>
-                                        <td align="center" valign="middle" class="itemPic_cell">
-                                            <!--<a href="services.php?i=2"><img src="images/itemPic_2.jpg" class="blockDisplay" /></a>-->
-                                            <a href="services/index.php"><img src="images/itemPic_2.jpg" class="blockDisplay" /></a>
-                                        </td>
-                                        </td>
-                                        <td align="center" valign="middle" class="itemPic_cell">
-                                            <!--<a href="services.php?i=3"><img src="images/itemPic_3.jpg" class="blockDisplay" /></a>-->
-                                            <a href="services/index.php"><img src="images/itemPic_3.jpg" class="blockDisplay" /></a>
-                                        </td>
-                                        </td>
-                                    </tr>
-                                    </table>
-                                </td>
-                            </tr>
-                            </table>
+<div id="home" class="content_body">
+    <ul id="service_indicator">
+	<li class="margin_right"><a href="services/index.php"><img src="images/itemPic_1.jpg"/></a></li>
+	<li class="margin_right"><a href="services/index.php"><img src="images/itemPic_2.jpg"/></a></li>
+	<li><a href="services/index.php"><img src="images/itemPic_3.jpg"/></a></li>
+    </ul>
+</div>
--- services/includes/_content.services.inc.php
+++ services/includes/_content.services.inc.php
@@ -1,38 +1,39 @@
-<h1><img src="../images/pageIndicator_services.jpg" /></h1>
-
-<div class="services_box">
-    <h2 class="service_type">Materials / Equipment Procurement</h2>
-    <ul class="services_list">
-	<!--<li><a href="?s=oil_tools">Oil Tools</a></li>-->
-	<li><a href="#">Oil Tools</a></li>
-	<li><a href="#">Electrical Equipment</a></li>
-	<li><a href="#">Rotating Equipment</a></li>
-	<li><a href="#">Heavy Equipment</a></li>
-	<li><a href="#">Inspection Equipment</a></li>
-	<li><a href="#">Instrumentation &amp; Control equipment</a></li>
-	<li><a href="#">Water Treatment Equipment</a></li>
-	<li><a href="#">Safety Materials</a></li>
-	<li><a href="#">Line pipes &amp; fittings</a></li>
-    </ul>
+<div id="services" class="content_body">
+    <div class="page_title"><img src="../images/pageIndicator_services.jpg"/></div>
+    <div class="services_box margin_right">
+	<h2 class="service_type">Materials / Equipment Procurement</h2>
+	<ul class="services_list">
+	    <!--<li><a href="?s=oil_tools">Oil Tools</a></li>-->
+	    <li><a href="#">Oil Tools</a></li>
+	    <li><a href="#">Electrical Equipment</a></li>
+	    <li><a href="#">Rotating Equipment</a></li>
+	    <li><a href="#">Heavy Equipment</a></li>
+	    <li><a href="#">Inspection Equipment</a></li>
+	    <li><a href="#">Instrumentation &amp; Control equipment</a></li>
+	    <li><a href="#">Water Treatment Equipment</a></li>
+	    <li><a href="#">Safety Materials</a></li>
+	    <li><a href="#">Line pipes &amp; fittings</a></li>
+	</ul>
+    </div>
+    <div class="services_box margin_right">
+	<h2 class="service_type">Consultancy Services</h2>
+	<ul class="services_list">
+	    <li><a href="#">Oilfield Automation Services</a></li>
+	    <li><a href="#">Logistics and Supply Chain Consulting</a></li>
+	    <li><a href="#">Strategy Consulting</a></li>
+	    <li><a href="#">Specialized Technical Consulting</a></li>
+	</ul>
+    </div>
+    <div class="services_box">
+	<h2 class="service_type">Onshore &amp; Offshore Engineering Services</h2>
+	<ul class="services_list">
+	    <li><a href="#">Marine Logistics</a></li>
+	    <li><a href="#">Corrosion Protection</a></li>
+	    <li><a href="#">Pipeline Coatings</a></li>
+	    <li><a href="#">Data Center Management</a></li>
+	    <li><a href="#">Call Center Management</a></li>
+	    <li><a href="#">Subsea Equipment &amp; Services</a>
+	</ul>
+    </div>
+    <div class="clear-float"></div>
 </div>
-<div class="services_box">
-    <h2 class="service_type">Consultancy Services</h2>
-    <ul class="services_list">
-	<li><a href="#">Oilfield Automation Services</a></li>
-	<li><a href="#">Logistics and Supply Chain Consulting</a></li>
-	<li><a href="#">Strategy Consulting</a></li>
-	<li><a href="#">Specialized Technical Consulting</a></li>
-    </ul>
-</div>
-<div class="services_box">
-    <h2 class="service_type">Onshore &amp; Offshore Engineering Services</h2>
-    <ul class="services_list">
-	<li><a href="#">Marine Logistics</a></li>
-	<li><a href="#">Corrosion Protection</a></li>
-	<li><a href="#">Pipeline Coatings</a></li>
-	<li><a href="#">Data Center Management</a></li>
-	<li><a href="#">Call Center Management</a></li>
-	<li><a href="#">Subsea Equipment &amp; Services</a>
-    </ul>
-</div>
-<div class="clear-float"></div>
