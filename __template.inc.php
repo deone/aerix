7366115576fb857ebfcbe4330b4dee8379863d30
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-05-17 13:51:54 +0100

    Template markup changed from table-based to valid XHTML divs.

--- __template.inc.php
+++ __template.inc.php
@@ -1,67 +1,29 @@
-<table cellspacing="0" cellpadding="0" id="pageContainer_table">
-    <tr>
-    	<td align="center" valign="top" id="pageContainer_cell">
-        	
-            <table cellspacing="0" cellpadding="0" id="pageFrame_table">
-            <tr>
-            	<td class="leftCell"></td>
-                <td align="left" valign="top" id="pageFrame_cell">
-                    <div id="navAndLogo_container">
-                    	<div id="navLinks_container">
-                        	<table cellspacing="0" cellpadding="0" class="navlinks_tableContainer">
-                            <tr>
-                            	<td align="left" valign="bottom">
-                                	<div class="floatLeft">
-                                    	<a href="index.php"><img src="images/link_home.jpg" class="blockDisplay" /></a>
-                                    </div>
-                                    <div class="floatLeft">
-                                    	<img src="images/link_sep.jpg" class="blockDisplay" />
-                                    </div>
-                                    <div class="floatLeft">
-                                    	<a href="aboutus.php"><img src="images/link_about.jpg" class="blockDisplay" /></a>
-                                    </div>
-                                    <div class="floatLeft">
-                                    	<img src="images/link_sep.jpg" class="blockDisplay" />
-                                    </div>
-                                    <div class="floatLeft">
-                                    	<a href="services/index.php"><img src="images/link_services.jpg" class="blockDisplay" /></a>
-                                    </div>
-                                    <div class="floatLeft">
-                                    	<img src="images/link_sep.jpg" class="blockDisplay" />
-                                    </div>
-                                    <div class="floatLeft">
-                                    	<a href="contact.php"><img src="images/link_contact.jpg" class="blockDisplay" /></a>
-                                    </div>
-                                </td>
-                            </tr>
-                            </table>
-                        </div>
-                        <div id="logo_container">
-                            <a href="index.php"><img src="images/logo.jpg" /></a>
-                        </div>
-                    </div><!--end navAndLogo_container-->
-                    
-                    <div id="majorBox_container">
-                    	<img src="images/mainPic.jpg" />
-                    </div>
-                    
-                    <div id="pageContentBody_container">
-                    	<div class="pageContentBody_cap"></div>
-                        <div class="pageContentBody_main">
-                        	<? @include( "includes/" . $contentFile ); ?>
-                        </div>                        
-                        <div class="pageContentBody_shoe"></div>
-                    </div>                    
-                </td>
-                <td class="rightCell"></td>
-            </tr>
-            <tr>
-            	<td class="leftCell bottomBg_cell"></td>
-                <td align="center" valign="middle" class="copyrightArea_cell"><img src="images/copyright_image.jpg" /></td>
-            	<td class="rightCell bottomBg_cell"></td>
-            </tr>
-            </table><!--end pageFrame_table-->
-            
-        </td><!--end pageContainer_cell-->
-    </tr>
-    </table><!--end pageContainer_table-->
+<div id="wrapper">
+    <div id="container">
+	<div id="header">
+	    <div id="navlogo">
+		<ul id="nav">
+		    <li><a href="index.php"><img src="images/link_home.jpg"/></a></li>
+		    <li><img src="images/link_sep.jpg"/></li>
+		    <li><a href="aboutus.php"><img src="images/link_about.jpg"/></a></li>
+		    <li><img src="images/link_sep.jpg"/></li>
+		    <li><a href="services/index.php"><img src="images/link_services.jpg"/></a></li>
+		    <li><img src="images/link_sep.jpg"/></li>
+		    <li><a href="contact.php"><img src="images/link_contact.jpg"/></a></li>
+		</ul>
+		<div id="logo">
+		    <a href="index.php"><img src="images/logo.jpg"/></a>
+		</div>
+		<div class="clear-float"></div>
+	    </div>
+	    <div id="mast">
+		<img src="images/mainPic.jpg" />
+	    </div>
+	</div>
+	<div class="content">
+	    <div class="gradient"></div>
+	    <? @include( "includes/" . $contentFile ); ?>
+	</div>
+	<div id="footer"><img src="images/copyright_image.jpg"/></div>
+    </div>
+</div>
