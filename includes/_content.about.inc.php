5ffeaf03e2caeb4d4ae0333d45c350400cd6bd9c
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-05-20 12:08:24 +0100

    Resized/Renamed/Deleted logos. We don't need about_menu.inc.php anymore, we can just insert the menu in the page. Added black and white images for effects.

--- includes/_content.about.inc.php
+++ includes/_content.about.inc.php
@@ -1,6 +1,11 @@
 <div id="about" class="content_body">
     <h1><img src="images/pageIndicator_about.jpg"/></h1>
-    <? include("about_menu.inc.php"); ?>
+    <ul class="menu">
+	<li><a class="first" href="#vision">Our Vision</a></li>
+	<li><a class="first" href="values.php">Our Values</a>
+	<li><a class="first" href="#partners_head">Our Partners</a></li>
+	<li><a class="first" href="#clients_head">Our Clients</a></li>
+    </ul>
     <div class="body">
 	<p>
 	    Aerix Global Solutions was incorporated to deliver home-grown but broad-based services 
@@ -14,18 +19,23 @@
 	through living our values.</p>
 	<h2 id="partners_head">Our Partners</h3>
 	<ul id="partners">
-	    <li><a href="http://www.elcometer.com" target="_blank"><img src="images/elcometerLogo.jpg"/></a></li>
-	    <li><a href="http://www.fike.com" target="_blank" class="clientLink"><img src="images/fike.jpg"/></a></li>
-	    <li><a href="http://www.bdsensors.com/home" target="_blank"><img src="images/bd_sensors.jpg"/></a></li>
-	    <li><a href="http://www.altrad.no/index.php" target="_blank"><img src="images/iwtm.jpg"/></a></li>
+	    <li><a id="elcometer" href="http://www.elcometer.com" target="_blank"></a></li>
+	    <li><a id="fike" href="http://www.fike.com" target="_blank" class="clientLink"></a></li>
+	    <li><a id="bdsensors" href="http://www.bdsensors.com/home" target="_blank"></a></li>
+	    <li><a id="iwtm" href="http://www.altrad.no/index.php" target="_blank"></a></li>
 	</ul>
+	<div class="clear-float"></div>
         <h2 id="clients_head">Our Clients</h2>
 	<ul id="clients">
-            <li><a href="http://www.total.com" target="_blank"><img src="images/totalLogo.jpg"/></a></li>
-            <li><a href="http://www.shell.com" target="_blank"><img src="images/shellLogo.jpg"/></a></li>
-            <li><a href="http://www.saipem.it" target="_blank"><img src="images/saipemLogo.jpg"/></a></li>
-            <li><a href="http://www.total.com" target="_blank"><img src="images/chevronLogo.jpg" /></a></li>
-            <li><a href="http://http://www.exxonmobil.com/corporate/" target="_blank"><img src="images/exxonmobilLogo.jpg"/></a></li>
+            <li><a id="total" href="http://www.total.com" target="_blank"></a></li>
+            <li><a id="shell" href="http://www.shell.com" target="_blank"></a></li>
+            <li><a id="saipem" href="http://www.saipem.it" target="_blank"></a></li>
+            <li><a id="chevron" href="http://www.chevron.com" target="_blank"></a></li>
+            <li><a id="mobil" href="http://http://www.exxonmobil.com/corporate/" target="_blank"></a></li>
+	</ul>
+	<div class="clear-float"></div>
+	<h3>Others are:</h3>
+	<ul>
             <li>Delta Afrik</li>
 	    <li>Hyundai Heavy Industries</li>
 	    <li>Seawolf Oilfield Service</li>
--- includes/about_menu.inc.php
+++
@@ -1,6 +0,0 @@
-<ul class="menu">
-    <li><a class="first" href="#vision">Our Vision</a></li>
-    <li><a class="first" href="values.php">Our Values</a>
-    <li><a class="first" href="#partners_head">Our Partners</a></li>
-    <li><a class="first" href="#clients_head">Our Clients</a></li>
-</ul>
