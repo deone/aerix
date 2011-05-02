8d977a6bc77dbeff8f120eccb919384a7982075e
Oladayo Osikoya <alwaysdeone@gmail.com>
2011-05-02 10:21:54 +0100

    Add the newest files I created back then

---
+++ includes/_content.values.inc.php
@@ -0,0 +1,104 @@
+<div id="values" class="content_body">
+    <h1>Our Values</h1>
+    <ul class="menu">
+	<li><a class="first" href="#customer_drive">Customer Drive</a></li>
+	<li><a class="first" href="#hse_mindset">HSE Mindset</a>
+	<li><a class="first" href="#people_and_teams">People and Teams</a></li>
+	<li><a class="first" href="#open_direct_dialogue">Open and Direct Dialogue</a></li>
+	<li><a class="first" href="#handson_management">Hands-on Management</a></li>
+	<li><a class="first" href="#delivering_results">Delivering Results</a></li>
+	<li><a class="first" href="#commitment_individual">Our commitment to individual customers</a></li>
+	<li><a class="first" href="#commitment_customers">Our commitment to our customers and partners</a></li>
+    </ul>
+    <dl class="body">
+	<h2 class="first" id="customer_drive">Customer Drive</h2>
+	<dt>Building customer trust is key to our business.</dt>
+	<dd>
+	    <p>After all, without customer trust and satisfaction, the rest does not matter. Good 
+	    customer references build our reputation &mdash; and the only way to achieve this is 
+	    by consistent and predictable performance. Our customers will recognise us for our execution excellence.</p>
+	    <p>We find new ways, always linked to real customer needs and business priorities.</p>
+	</dd>
+
+	<h2 id="hse_mindset">HSE Mindset</h2>
+	<dt>We take personal responsibility for HSE because we care.</dt>
+	<dd>
+	    <p>All incidents can be prevented. We continuously strive for zero accidents to personnel, material 
+	    and non-material assets.</p>
+	    <p>We focus on employee health and on continuously improving the work environment.</p>
+	    <p>We conduct our operations through efficient use of materials and energy, with minimum waste 
+	    and damage to the environment.</p>
+	    <p>We design products and services to have no undue environmental impact, to be safe and to 
+	    be efficient in consuming energy and natural resources. We seek to ensure that our products can be 
+	    disposed of safely.</p>
+	</dd>
+
+	<h2 id="people_and_teams">People and Teams</h2>
+	<dt>All our major achievements are team efforts.</dt>
+	<dd>
+	    <p>In the end, it comes down to the talent and motivation of you and me. Delivering strong results 
+	    is impossible without a highly capable workforce.</p>
+	    <p>We learn on the job, through challenging tasks, coaching and training.</p>
+	    <p>Development of people and teams in our company has one purpose &mdash; to create a foundation for 
+	    long-term sustainable value creation through efficient project execution and sound business operations.</p>
+	    <p>We respect and encourage diversity and build strong, energized and effective teams &mdash; and we have 
+	    fun together, making us even better.</p>
+	    <p>Our goal is to be the preferred employer in our industry.</p>
+	</dd>
+
+	<h2 id="open_direct_dialogue">Open and direct dialogue</h2>
+	<dt>We encourage early and honest communication.</dt>
+	<dd>
+	    <p>We listen hard and talk straight &mdash; no sugar coating, no filters.</p>
+	    <p>We value early, accurate and reliable communication &mdash; after all, the first problem we encounter is 
+	    usually the easiest one to solve.</p>
+	    <p>We challenge each other. The best decisions are taken when different opinions and different cultures 
+	    meet in open and direct dialogue.</p>
+	    <p>We expect the highest standards of ethical behaviour and integrity &mdash; from all of us, everywhere.</p>
+	</dd>
+
+	<h2 id="handson_management">Hands-on management</h2>
+	<dt>We know our business and get things done.</dt>
+	<p>Once decisions are made, we combine all our efforts and focus all our energies on execution.</p>
+	<p>We are accountable and solutions-oriented, focusing on the right details at the right time. We follow through and ensure accountability.</p>
+	<p>We believe in empowering the people close to the action to take responsibility. Hands-on does not mean hands-in.</p>
+	<p>We stimulate entrepreneurship and challenge bureaucracy, complicated hierarchies and "silo" mentalities. We are one Aerix Global Solutions.</p>
+
+	<h2 id="delivering_results">Delivering Results</h2>
+	<dt>We deliver consistently and strive to beat our goals.</dt>
+	<dd>
+	    <p>When doing a job, we understand both the risks and opportunities involved and know how to manage them. 
+	    We take pride in delivering as we promise.</p>
+	    <p>Making money creates new opportunities and the resources for going forward, both for us and for 
+	    our customers and partners. Commercial edge benefits us all.</p>
+	    <p>We reward performance - what you achieve - and alignment with our values - how you behave.</p>
+	</dd>
+
+	<h2 id="commitment_individual">Our commitment to individual competence</h2>
+	<dd>
+	    <p>Aerix Global Solutions' deliveries are the sum of the expertise and experience of many individuals. 
+	    This diversity of personal capabilities and the interaction between them liberates energy and generates 
+	    new ideas, creative solutions and lasting value.</p>
+	    <p>Aerix Global Solutions actively strives to attract and develop talented individuals who understand our 
+	    customers’ needs and requirement and who can produce and deliver the best solutions.</p>
+	    <p>Aerix Global Solutions respects diversity and encourages the building of teams in which energy, 
+	    enthusiasm and creativity can be unlocked through diversification.</p>
+	    <p>We are committed to employment policies which are open and fair and without distinction of any kind such as 
+	    race, colour, sex, language, religion, political or other opinion, national or social origin or other status. 
+	    Aerix Global Solutions employees are entitled to a workplace which is safe and is free from harassment.</p>
+	    <p>Aerix Global Solutions supports the United Nations Universal Declaration of Human Rights and the standards 
+	    advised by the International Labour Organization.</p>
+	</dd>
+
+	<h2 id="commitment_customers">Our commitment to our customers and partners</h2>
+	<dd>
+	    <p>Aerix Global Solutions is committed to always go the extra mile. We find new ways, always linked to real 
+	    customer needs and business priorities. After all, without customer trust and satisfaction, the rest does not matter.</p>
+	    <p>Aerix Global Solutions has a good relationship with its customers, and will always strive to 
+	    make this even better. A number of initiatives have been pursued over the past couple of years to 
+	    strengthen such relations.</p>
+	    <p>We are always keen to listen to our customers and learn from their feedback. We regularly carry out customer 
+	    surveys, and encourage our customers to provide their input at any given time.</p>
+    </div>
+    <div class="clear-float"></div>
+</dl>
