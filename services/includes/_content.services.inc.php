21b3136ff8160e3a01f2196830a501c983a3fe28
Oladayo Osikoya <alwaysdeone@gmail.com>
2011-05-02 10:57:12 +0100

    Add new information to services page. Remove obsolete info

--- services/includes/_content.services.inc.php
+++ services/includes/_content.services.inc.php
@@ -1,38 +1,60 @@
 <div id="services" class="content_body">
     <h1><img src="../images/pageIndicator_services.jpg"/></h1>
     <div class="box margin_right">
-	<h2>Materials / Equipment Procurement</h2>
+	<h2>Engineering Services</h2>
 	<ul>
-	    <!--<li><a href="?s=oil_tools">Oil Tools</a></li>-->
-	    <li><a href="#">Oil Tools</a></li>
-	    <li><a href="#">Electrical Equipment</a></li>
-	    <li><a href="#">Rotating Equipment</a></li>
-	    <li><a href="#">Heavy Equipment</a></li>
-	    <li><a href="#">Inspection Equipment</a></li>
-	    <li><a href="#">Instrumentation &amp; Control equipment</a></li>
-	    <li><a href="#">Water Treatment Equipment</a></li>
-	    <li><a href="#">Safety Materials</a></li>
-	    <li><a href="#">Line pipes &amp; fittings</a></li>
+	    <li><a href="#">Pipework</a></li>
+	    <li><a href="#">Radiation Detection &amp; Monitoring</a></li>
+	    <li><a href="#">Steelwork/Steel Structures</a></li>
+	    <li><a href="#">Heavy duty Diesel Engines</a></li>
+	    <li><a href="#">Welding Services</a></li>
+	    <li><a href="#">Industrial Cleaning Services</a></li>
+	    <li><a href="#">Complete Turnkey Solutions</a></li>
+	    <li><a href="#">Subsea Solutions</a></li>
+	    <li><a href="#">Corrosion Protection &amp; Inspection</a></li>
+	    <li><a href="#">NDT Testing</a></li>
+	    <li><a href="#">Instrument Testing/Calibration Services</a></li>
+	    <li><a href="#">Surface Treatment Inspection</a></li>
+	    <li><a href="#">Crane Management/Certification Services</a></li>
+	    <li><a href="#">Drilling Services</a></li>
+	    <li><a href="#">Armored Vehicles Maintenance</a></li>
+	    <li><a href="#">Procurement</a>
+		<ul>
+		    <li><a href="#">Valves</a></li>
+		    <li><a href="#">Seamless Pipes &amp; Tubes</a></li>
+		    <li><a href="#">Atmospheric &amp; Deep sea Diving Systems</a></li>
+		    <li><a href="#">Subsea Pipeline Connectors</a></li>
+		    <li><a href="#">Fire/Gas Detection/Protection Systems</a></li>
+		</ul>
+	    </li>
 	</ul>
     </div>
     <div class="box margin_right">
-	<h2 class="service_type">Consultancy Services</h2>
+	<h2 class="service_type">Marine Services</h2>
 	<ul>
-	    <li><a href="#">Oilfield Automation Services</a></li>
-	    <li><a href="#">Logistics and Supply Chain Consulting</a></li>
-	    <li><a href="#">Strategy Consulting</a></li>
-	    <li><a href="#">Specialized Technical Consulting</a></li>
+	    <li><a href="#">Diving Support Vessels</a></li>
+	    <li><a href="#">FPSO/FSO Maintenance</a></li>
+	    <li><a href="#">Ship Repair Services</a></li>
+	    <li><a href="#">Barges</a></li>
+	    <li><a href="#">Offshore Tank Cleaning Services</a></li>
+	    <li><a href="#">Tank Repairs/Modification</a></li>
+	    <li><a href="#">Refurbishments/Galley/HVAC</a></li>
+	    <li><a href="#">Offshore Accomodation Modules</a></li>
+	    <li><a href="#">Marine Life-saving Systems</a></li>
 	</ul>
     </div>
     <div class="box">
-	<h2>Onshore &amp; Offshore Engineering Services</h2>
+	<h2>Manpower Supply</h2>
 	<ul>
-	    <li><a href="#">Marine Logistics</a></li>
-	    <li><a href="#">Corrosion Protection</a></li>
-	    <li><a href="#">Pipeline Coatings</a></li>
-	    <li><a href="#">Data Center Management</a></li>
-	    <li><a href="#">Call Center Management</a></li>
-	    <li><a href="#">Subsea Equipment &amp; Services</a>
+	    <li><a href="#">Project Managers</a></li>
+	    <li><a href="#">Construction Managers</a></li>
+	    <li><a href="#">Commissioning Managers</a></li>
+	    <li><a href="#">QA/QC Managers</a></li>
+	    <li><a href="#">HSE Managers</a></li>
+	    <li><a href="#">Operation &amp; Maintenance Managers</a>
+	    <li><a href="#">Inspectors</a>
+	    <li><a href="#">Project &amp; Design Engineers</a>
+	    <li><a href="#">Administration &amp; Finance Personnel</a>
 	</ul>
     </div>
     <div class="clear-float"></div>
